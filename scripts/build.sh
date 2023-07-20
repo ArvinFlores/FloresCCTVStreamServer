67e723146fd117ffe1d4c48809306733e92c0f66
Arvin Flores <arvinflores255@gmail.com>
2023-07-20 10:45:16 -0700

    Update build script to skip rebuilding when build dir exists

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,32 +11,31 @@ log() {
   echo -e "====================================================================================================\n"
 }
 
-if [[ -d $WEB_ASSETS_PATH ]]; then
-  log "Removing $ASSETS_DIR repo as it might be stale"
+if [[ -d $CURR_DIR/build ]]; then
+  log "build/ folder exists already, skipping build... remove the build/ folder to rebuild the assets if desired"
+else
+  log "Cloning the web assets repo into $PROJECTS_PATH"
+  cd $PROJECTS_PATH
+  git clone git@github.com:ArvinFlores/FloresCCTVWebAssets.git
+  cd $ASSETS_DIR
+
+  log "Installing the project dependencies for $ASSETS_DIR"
+  npm install
+
+  log "Building the production web assets for $ASSETS_DIR"
+  echo "CAMERA_IP=$(hostname -I | awk '{print $1}')" >> .env
+  echo "JANUS_URL=$FLORESCCTV_JANUS_URL$FLORESCCTV_JANUS_ROOT" >> .env
+  echo "JANUS_ROOM=$FLORESCCTV_JANUS_ROOM" >> .env
+  npm run build
+
+  log "Copying the production web assets into $CURR_DIR/build"
+  mkdir -p $CURR_DIR/build
+  rm -f $CURR_DIR/build/*
+  cp -r build/* $CURR_DIR/build
+  cd $CURR_DIR
+
+  log "Removing $WEB_ASSETS_PATH"
   rm -rf $WEB_ASSETS_PATH
+
+  log "Streaming server is ready to run in production"
 fi
-
-log "Cloning the web assets repo into $PROJECTS_PATH"
-cd $PROJECTS_PATH
-git clone git@github.com:ArvinFlores/FloresCCTVWebAssets.git
-cd $ASSETS_DIR
-
-log "Installing the project dependencies for $ASSETS_DIR"
-npm install
-
-log "Building the production web assets for $ASSETS_DIR"
-echo "CAMERA_IP=$(hostname -I | awk '{print $1}')" >> .env
-echo "JANUS_URL=$FLORESCCTV_JANUS_URL$FLORESCCTV_JANUS_ROOT" >> .env
-echo "JANUS_ROOM=$FLORESCCTV_JANUS_ROOM" >> .env
-npm run build
-
-log "Copying the production web assets into $CURR_DIR/build"
-mkdir -p $CURR_DIR/build
-rm -f $CURR_DIR/build/*
-cp -r build/* $CURR_DIR/build
-cd $CURR_DIR
-
-log "Removing $WEB_ASSETS_PATH"
-rm -rf $WEB_ASSETS_PATH
-
-log "Streaming server is ready to run in production"
