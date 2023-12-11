92514e4a6337ae77ce0887a9b39746af89780f82
Arvin Flores <arvinflores255@gmail.com>
2023-12-10 19:17:01 -0800

    update video node for upload_recording script

--- scripts/upload_recording.sh
+++ scripts/upload_recording.sh
@@ -6,7 +6,7 @@ ext="mp4"
 timestamp=$(date +"%m-%d-%Y_%I:%M%p")
 filename="$(pwd)/$timestamp.$ext"
 
-ffmpeg -i /dev/video4 -t 10 "$filename"
+ffmpeg -i /dev/video3 -t 10 "$filename"
 
 curl -X POST -v -F file=@"$filename" "$FLORESCCTV_API_URL/api/recordings"
 
